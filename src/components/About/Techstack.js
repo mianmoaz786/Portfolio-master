e54cbf09afbb06949215c351e8f7c05a202e94c3
mianmoaz786 <136153007+mianmoaz786@users.noreply.github.com>
2024-07-11 23:53:32 +0500

    nee update
    updated successfully

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { DiJavascript1, DiReact, DiPython, DiAndroid } from "react-icons/di";
+import {  DiReact, DiPython, DiAndroid } from "react-icons/di";
 import { SiFirebase } from "react-icons/si";
 
 function Techstack() {
