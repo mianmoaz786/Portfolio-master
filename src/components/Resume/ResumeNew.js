02b4621110d4d1505cbd5fb2023f5cf36c5b133b
Moaz Raheem <moazraheem391@gmail.com>
2025-01-26 22:20:28 +0500

    resume updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Moaz_Raheem.pdf"; // Update the path if necessary
+import pdf from "../../Assets/Moaz Dev.pdf"; 
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
